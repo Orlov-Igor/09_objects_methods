d61c3c322849ba1dcf962abccee79155be751311
igor_orlov <thoronion@gmail.com>
2020-07-25 15:36:23 +0300

    New try

--- src/index.js
+++ src/index.js
@@ -1,111 +1,71 @@
-let toDoList = [
-        {
-        name: "Task №1",
-        dateOfCreation: "02/07/2020",
-        status: "Completed",
-        text: "Visit a dentist",
-        },
-     
-        {
-        name: "Task №2",
-        dateOfCreation: "05/07/2020",
-        status: "Active",
-        text: "Cut nose hair",
-        },
 
-        {
-        name: "Task №3",
-        dateOfCreation: "10/07/2020",
-        status: "Active",
-        text: "Bake a cake",
-        },
+let listObject = {
+    toDoList: [
+    {
+    name: "Task №1",
+    dateOfCreation: "02/07/2020",
+    status: "Completed",
+    text: "Visit a dentist",
+    },
+ 
+    {
+    name: "Task №2",
+    dateOfCreation: "05/07/2020",
+    status: "Active",
+    text: "Cut nose hair",
+    },
+
+    {
+    name: "Task №3",
+    dateOfCreation: "10/07/2020",
+    status: "Active",
+    text: "Bake a cake",
+    },
+    ],
     
-]
+}
+
+listObject.addTask = function() {
+    let newTask = {
+        name: "Task number",
+        dateOfCreation: "Current date",
+        status: "Active",
+        text: "Some text",
+        }
+    listObject.toDoList.push(newTask);
+};
   
-function addTask(arr) {
-    let newTask = prompt("Enter new task");
-    let counter = 0;
-        for (let i = 0; i <  arr.length; i++) {
-      
-            if (arr[i].text === newTask ) {
-            counter ++;
-        };
-    }
+listObject.addTask();
+console.log(listObject);
 
-    if (counter < 1) {
-        let newTaskObj = {
-            name: "Task №" + (arr.length + 1),
-            dateOfCreation: "Current date",
-            status: "Active",
-            text: newTask,  
-        };
-        arr.push(newTaskObj);
+
+listObject.deleteTask = function(text, confirmation) {
+   if (confirmation) {
+       let task = listObject.toDoList.find(function(item) {
+       return item.text === text;
+    });
+    let i = listObject.toDoList.indexOf(task);
+    listObject.toDoList.splice(i, 1);  
+   }; 
+};
+
+console.log(listObject.deleteTask("Cut nose hair", true));
+console.log(listObject);
+
+
+listObject.findDublicate = function(text) {
+    let task = listObject.toDoList.find(function(item) {
+        return item.text === text;
+    });
+    if (task !== undefined) {
+        return true;
+    } else {
+        return false;
     };
-return arr;
 };
 
-console.log(addTask(toDoList));
+console.log(listObject.findDublicate("Cut nose hair"));
 
 
-function deleteTask(arr, name) {
-    for (let i = 0; i <  arr.length; i++) {
-        
-        if (name === arr[i].name) {
-            const confirmation = confirm("Вы действительно хотите удалить " + arr[i].name + " ?");
-            
-            if (confirmation) {
-                delete arr[i];
-            }
-        };
-    };
-return arr;
-};
 
-console.log(deleteTask(toDoList, "Task №2"));
-
-
-function editTask(arr, name) {
-    for (let i = 0; i <  arr.length; i++) {
-        
-        if (name === arr[i].name) {
-            let changes = prompt("Введите новый текст");
-            
-            if(confirm("Сохранить изменения текста?")) {
-                arr[i].text = changes;
-            };   
-        };
-    };
-return arr;
-};
-
-console.log(editTask(toDoList, "Task №2"));
-
-
-function getInfo(arr) {
-    let counterAct = 0;
-    let counterCmpl = 0;
-    
-        for (let i = 0; i <  arr.length; i++) {
-        
-            if (arr[i].status === "Active") {
-                counterAct++;
-            } else {
-                counterCmpl++;
-            };    
-        };
-
-return "Всего задач выполнено: " + counterCmpl + ", активных: " + counterAct;
-};
-
-console.log(getInfo(toDoList));
-
-
-function makeSealed(arr) {
-    
-    for (let i = 0; i <  arr.length; i++) {
-       
-        Object.seal(arr[i]);
-    }
-return arr;
-};
 
